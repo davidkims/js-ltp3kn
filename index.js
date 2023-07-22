c95c0903f91b441f287b1fdb78f9e00c9e8f9525
davidkims <120926381+davidkims@users.noreply.github.com>
2023-07-22 17:36:08 +0900

    2 files modified, 1 deleted

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-// Sample validation function
-function validate() {
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
+const nodemailer = require('nodemailer');
 
-  if(username == "myUsername" && password == "myPassword"){
-      alert("Login successful");
-      // Add code here to transition to your application
-  }else{
-      alert("Login failed. Check your username and password.");
+let transporter = nodemailer.createTransport({
+    service: 'gmail', // 사용하려는 이메일 서비스 (여기서는 gmail)
+    auth: {
+      user: 'your-email@gmail.com', // 이메일 계정
+      pass: 'your-password' // 이메일 계정의 비밀번호
+    }
+});
+
+let mailOptions = {
+    from: 'your-email@gmail.com', // 보내는 이메일 주소
+    to: 'recipient-email@gmail.com', // 받는 이메일 주소
+    subject: 'Testing email with Node.js', // 메일 제목
+    text: 'Hello, this is a test email from Node.js!' // 메일 본문
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
   }
-}
+});
